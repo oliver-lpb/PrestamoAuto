c5d5f9dd04828150288b8213a4920329c2ef71e9
oliver-lpb <82127783+oliver-lpb@users.noreply.github.com>
2022-09-28 09:25:05 -0600

    Version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './componente/add-user/add-user.component';
+import { ListUserComponent } from './componente/list-user/list-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    ListUserComponent
   ],
   imports: [
     BrowserModule,
